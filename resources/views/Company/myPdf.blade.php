0c4efce385bb9b11bb1558f9f124d8b049f033dd
LeoZacchi <leonardo_zacchi@hotmail.com>
2022-04-04 14:57:08 -0300

    falta gerar pdf

--- resources/views/Company/myPdf.blade.php
+++ resources/views/Company/myPdf.blade.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
     <title>Sucesso Empregos Currículos</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
 </head>
 
 <body>
@@ -17,19 +17,19 @@
             <img src="img/user-image.png" alt="" />
         </div>
     </div>
-
-    <h2>{{ $curriculum->name }}</h2>
+    <h2>{{ $data->name}}</h2>
 
     <label>Considerações Adicionais</label>
-    <p>{{ $curriculum->additional_considerations }}</p>
+    <p>{{ $data->additional_considerations }}</p>
 
     <div class="row">
         <div class="col">
             <h3>Informações de contato</h3>
             <label>Email</label>
-            <p>{{ $curriculum->email }}</p>
+            <p>{{ $data->email }}</p>
         </div>
     </div>
+
 
 </body>
 
