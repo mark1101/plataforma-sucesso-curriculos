2a38c8956527074a97e750dde0a7d7897d6bee01
Leonardo Losso <leowlosso@gmail.com>
2022-08-23 10:07:26 -0300

    admin login front OK

--- resources/views/Admin/login-admin.blade.php
+++ resources/views/Admin/login-admin.blade.php
@@ -1,73 +1,60 @@
-@extends('Layout.tertiary-login')
+@extends('Layout.tertiary-loginadm')
 
-@section('title', 'Login Admin')
+@section('title', 'Painel administrativo')
 
 @section('content')
 
-    <!--------- Hero area start --------->
-    <section class="hero__area presa-hero">
-        <div class="hero__shape-2">
-            <img src="{{ asset('img/shapes/shape-3.png') }}" alt="">
-        </div>
-        <div class="container position-relative">
-            <div class="row align-items-center">
-                <div class="col-md-6">
-                    <div class="hero__content__wrapp">
-                        <p class="text-md">Tenha acesso a todas informações relativas da plataforma sucesso empregos 
-                        </p>
-                    </div>
+<!--------- Hero area start --------->
+<section class="hero__area presa-hero">
+    <div class="hero__shape-2">
+        <img src="{{ asset('img/shapes/shape-3.png') }}" alt="">
+    </div>
+    <div class="container position-relative">
+        <div class="row align-items-center">
+            <div class="col-md-6">
+                <div class="hero__content__wrapp">
+                    <p class="text-md">Painel administrativo para a plataforma Sucesso Empregos. A visualização/utilização e eventual compartilhamento dos dados referentes aos usuários da plataforma é de responsabilidade do administrador.
+                    </p>
                 </div>
-                <div class="note-thumb note-thumb-2">
-                    <img src="{{ asset('img/thumb/thumb-3.png') }}" alt="">
-                </div>
-                <div class="col-md-6">
-                    <div class="hero__form__wrapper">
-                        <h4 class="text-red">Bem Vindo Admin</h4>
-                        <form method="POST" action="{{ route('login') }}">
-                            @csrf
-                            <div class="single__input__item">
-                                <label for="email">{{ __('Digite seu email') }}</label>
+            </div>
+            <div class="col-md-6">
+                <div class="hero__form__wrapper">
+                    <h4 style="text-align: center;" class="text-red">Área de Login</h4>
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
+                        <div class="single__input__item">
+                            <label for="email">{{ __('Digite o email de administrador') }}</label>
 
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
-                                    name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                            @error('email')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
 
-                            </div>
-                            <div class="single__input__item">
-                                <label for="password">{{ __('Sua senha') }}</label>
+                        </div>
+                        <div class="single__input__item">
+                            <label for="password">{{ __('Digite a senha administradora') }}</label>
 
-                                <input id="password" type="password"
-                                    class="form-control @error('password') is-invalid @enderror" name="password" required
-                                    autocomplete="current-password">
+                            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                            @error('password')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
 
-                            </div>
-                            <div class="hero__form__bottom d-flex justify-content-between pt-3">
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Recuperar senha') }}
-                                    </a>
-                                @endif
-                            </div>
-                            <div class="hero__form__bottom d-flex justify-content-between pt-3">
-                                <button type="submit" class="btn-red" style="width: 100%!important;">Entrar</button>
-                            </div>
-                        </form>
-                    </div>
+                        </div>
+                        <div class="hero__form__bottom d-flex justify-content-between pt-3">
+                            <button type="submit" class="btn-red" style="width: 100%!important;">Entrar</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
-    </section>
-    <!--------- Hero area end --------->
+    </div>
+</section>
+<!--------- Hero area end --------->
 
 @endsection
